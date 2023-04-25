3e96e6aa406117391237c1b01ecb3a7976675f4b
laoal11 <pascal.lage@t-oline.de>
2023-04-25 21:04:58 +0200

    add exception

--- src/main/java/org/example/simulator/SimServiceSimulator.java
+++ src/main/java/org/example/simulator/SimServiceSimulator.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class SimServiceSimulator {
 
-    List<Sim> getForObject(String url, Class<?> responseType) {
+    List<Sim> getForObject(String url, Class<?> responseType) throws Exception {
         return Arrays.asList(new Sim("491512364", "262019876542210"), new Sim("491512365","262019876543210"));
     }
 }
